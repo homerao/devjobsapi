cfc429f2f064dc361738570a07fbe0211a9cd435
Homero <homerao@gmail.com>
2021-05-31 10:10:03 -0300

    implementando serviços

--- routes/api.index.routes.js
+++ routes/api.index.routes.js
@@ -1,7 +1,20 @@
 const indexRouter = require('express').Router()
 const desenvolvedoresRoutes = require('./desenvolvedores.routes')
+const candidaturaRoutes = require('./candidatura.routes')
+const contaRoutes = require('./conta.routes')
+const contatoRoutes = require('./contato.candidatura.routes')
+const empresaRoutes = require('./empresa.routes')
+const recuperarContaRoutes = require('./recuperar.conta.routes')
+const vagaRoutes = require('./vagas.routes')
 
 indexRouter.use('/desenvolvedores',desenvolvedoresRoutes)
+indexRouter.use('/candidaturas',candidaturaRoutes)
+indexRouter.use('/contas',contaRoutes)
+indexRouter.use('/contatos',contatoRoutes)
+indexRouter.use('/empresas',empresaRoutes)
+indexRouter.use('/recuperar.contas',recuperarContaRoutes)
+indexRouter.use('/vagas',vagaRoutes)
+
 
 indexRouter.get('/', (req, res, next)=>{
      res.send("Api funcionando")
