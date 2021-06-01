3f96769f1a0bd702fc00415e35b3056c3cf9ad90
Homero <homerao@gmail.com>
2021-06-01 16:12:47 -0300

    implementando services

--- services/candidatura.service.js
+++ services/candidatura.service.js
@@ -1,1 +1,30 @@
 const repository = require('../repositories/candidatura.repository')
+
+class CandidaturaService {
+
+    async create(candidatura){
+
+    }
+
+    async update(candidatura){
+
+    }
+    async findByPk(pk){
+
+    }
+    async findByEmail(email){
+
+    }
+
+    async findAll(limit, offset){
+
+    }
+
+    async findall(){
+
+    }
+
+    async count(){
+        
+    }
+}
--- services/conta.service.js
+++ services/conta.service.js
@@ -1,1 +1,32 @@
 const repository = require('../repositories/conta.repository')
+
+
+
+class ContaService {
+
+    async create(conta){
+
+    }
+
+    async update(conta){
+
+    }
+    async findByPk(pk){
+
+    }
+    async findByEmail(email){
+
+    }
+
+    async findAll(limit, offset){
+
+    }
+
+    async findall(){
+
+    }
+
+    async count(){
+        
+    }
+}
